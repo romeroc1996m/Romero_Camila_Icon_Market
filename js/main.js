0f67741057f71ba788b19232062c4e113f80abeb
romeroc1996m <camilaromero.9603@gmail.com>
2023-01-21 20:34:39 -0500

    adding changes
    adding changes

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 console.log('Welcome');
 
+//1
 const iconmarket = document.querySelector('#bread_icon');
 
 function logThisId() {
@@ -7,4 +8,87 @@ function logThisId() {
 }
 
 iconmarket.addEventListener('click', logThisId);
+
+//2
+const iconmarket0 = document.querySelector('#candy_icon');
+
+function logThisId() {
+    console.log('clicked on this element:', this.id);
+}
+
+iconmarket0.addEventListener('click', logThisId);
+
+//3
+const icon1 = document.querySelector('#cherry_icon');
+
+function logThisId() {
+    console.log('clicked on this element:', this.id);
+}
+
+icon1.addEventListener('click', logThisId);
+
+//4
+const icon2 = document.querySelector('#coffee_icon');
+
+function logThisId() {
+    console.log('clicked on this element:', this.id);
+}
+
+icon2.addEventListener('click', logThisId);
+
+//4
+const icon3 = document.querySelector('#cupcake_icon');
+
+function logThisId() {
+    console.log('clicked on this element:', this.id);
+}
+
+icon3.addEventListener('click', logThisId);
+
+//5
+const icon4 = document.querySelector('#donut_icon');
+
+function logThisId() {
+    console.log('clicked on this element:', this.id);
+}
+
+icon4.addEventListener('click', logThisId);
+
+//6
+const icon5 = document.querySelector('#icecream_icon');
+
+function logThisId() {
+    console.log('clicked on this element:', this.id);
+}
+
+icon5.addEventListener('click', logThisId);
+
+//7
+const icon6 = document.querySelector('#lollipop_icon');
+
+function logThisId() {
+    console.log('clicked on this element:', this.id);
+}
+
+icon6.addEventListener('click', logThisId);
+
+//8
+const icon7= document.querySelector('#milk_icon');
+
+function logThisId() {
+    console.log('clicked on this element:', this.id);
+}
+
+icon7.addEventListener('click', logThisId);
+
+//9
+const icon8 = document.querySelector('#strawberry_icon');
+
+function logThisId() {
+    console.log('clicked on this element:', this.id);
+}
+
+icon8.addEventListener('click', logThisId);
+
+
 
